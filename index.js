df29647f423860d977ffbe8f97f0b883ec60d84a
Bekki Young <moonbladez@hotmail.com>
2020-02-16 18:52:19 +0100

    horizontal and vertical walls added for maze

--- index.js
+++ index.js
@@ -1,64 +1,81 @@
-const {
-    Engine,
-    Render,
-    Runner,
-    World,
-    Bodies,
-    MouseConstraint,
-    Mouse
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+//config
+const cells = 3;
+const width = 600;
 const height = 600;
+
 const engine = Engine.create();
-const {
-    world
-} = engine;
+const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false,
-        width,
-        height
-    }
+	element: document.body,
+	engine: engine,
+	options: {
+		wireframes: false,
+		width,
+		height,
+	},
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
-
 //walls
 const walls = [
-    //top
-    Bodies.rectangle(400, 0, 800, 40, {
-        isStatic: true
-    }),
-    //bottom
-    Bodies.rectangle(400, 600, 800, 40, {
-        isStatic: true
-    }),
-    //leftside
-    Bodies.rectangle(0, 300, 40, 600, {
-        isStatic: true
-    }),
-    //rightside
-    Bodies.rectangle(800, 300, 40, 600, {
-        isStatic: true
-    }),
+	//top
+	Bodies.rectangle(width / 2, 0, width, 40, {
+		isStatic: true,
+	}),
+	//bottom
+	Bodies.rectangle(width / 2, height, width, 40, {
+		isStatic: true,
+	}),
+	//leftside
+	Bodies.rectangle(0, height / 2, 40, height, {
+		isStatic: true,
+	}),
+	//rightside
+	Bodies.rectangle(width, height / 2, 40, height, {
+		isStatic: true,
+	}),
 ];
-World.add(world, walls)
+World.add(world, walls);
 
-//random shapes
-for (let i = 0; i < 50; i++) {
-    //random shape
-    if (Math.random() > 0.5) {
-        //random position starting
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50))
-    } else {
-        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35))
-    }
+//maze generating
+const grid = Array(cells)
+	.fill(null)
+	.map(() => Array(cells).fill(false));
 
-}
+//vertical lines
+const verticals = Array(cells)
+	.fill(null)
+	.map(() => Array(cells - 1).fill(false));
+
+//horizontal lines
+const horizontals = Array(cells - 1)
+	.fill(null)
+	.map(() => Array(cells).fill(false));
+
+//find random starting spot
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+console.log(startRow, startColumn);
+
+const iterateCells = (row, column) => {
+    //if visited cell at [row, column] then return
+
+    //mark this cell as visited in const grid.
+
+    //assemble random ordered list of neighbours
+
+    //for each neighbour...
+
+    //see if neighbour is out of bounds
+
+    //check to see if we have visited neighbour, continue to next neighbour
+
+    //remove wall from either horizonal or veritcal
+
+    //visit that next cell
+};
+
+iterateCells(startRow, startColumn);
